8c42198ffe0828e2afa16eec7a432b9b3de66910
Nathan Cheng <nthn.wei@gmail.com>
2024-07-17 17:54:45 -0400

    Increase h2 font weight

--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
     <main className="container mx-auto flex min-h-screen flex-col gap-y-12 p-4">
       <h1 className="text-2xl">Upwage Question Builders</h1>
       <section>
-        <h2 className="mb-6 text-xl text-black/70 dark:text-white/70">
+        <h2 className="mb-6 text-xl font-semibold text-black/70 dark:text-white/70">
           Yes/No
         </h2>
         <YesNo />
       </section>
       <section>
-        <h2 className="mb-6 text-xl text-black/70 dark:text-white/70">
+        <h2 className="mb-6 text-xl font-semibold text-black/70 dark:text-white/70">
           Number
         </h2>
         <NumberQuestion />
