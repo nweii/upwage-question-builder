40f3c06754edc4aac2d0737b478cde084c09b9ed
Nathan Cheng <nthn.wei@gmail.com>
2024-08-12 19:33:37 -0400

    fix: better tab styling for sm breakpoints

--- app/page.js
+++ app/page.js
@@ -3,11 +3,14 @@ import QuestionForm from "/components/QuestionForm";
 
 export default function Home() {
   return (
-    <main className="container mx-auto flex min-h-screen flex-col gap-y-12 p-4">
+    <main className="container mx-auto flex min-h-screen max-w-screen-lg flex-col gap-y-12 p-4">
       <h1 className="text-2xl">Upwage Question Builder</h1>
       <Tabs defaultValue="yes_no" className="w-full">
-        <TabsList className="grid w-full grid-cols-4">
-          <TabsTrigger value="yes_no" className="gap-1">
+        <TabsList className="grid h-auto w-full grid-cols-4">
+          <TabsTrigger
+            value="yes_no"
+            className="flex flex-col items-center justify-center gap-1"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -15,17 +18,20 @@ export default function Home() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-badge-check"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="sm:mb-1"
             >
               <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
               <path d="m9 12 2 2 4-4" />
             </svg>
-            Yes/No
+            <span className="text-xs">Yes/No</span>
           </TabsTrigger>
-          <TabsTrigger value="number" className="gap-1">
+          <TabsTrigger
+            value="number"
+            className="flex flex-col items-center justify-center gap-1"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -33,19 +39,22 @@ export default function Home() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-hash"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="sm:mb-1"
             >
               <line x1="4" x2="20" y1="9" y2="9" />
               <line x1="4" x2="20" y1="15" y2="15" />
               <line x1="10" x2="8" y1="3" y2="21" />
               <line x1="16" x2="14" y1="3" y2="21" />
             </svg>
-            Number
+            <span className="text-xs">Number</span>
           </TabsTrigger>
-          <TabsTrigger value="single_select" className="gap-1">
+          <TabsTrigger
+            value="single_select"
+            className="flex flex-col items-center justify-center gap-1"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -53,17 +62,20 @@ export default function Home() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-circle-dot"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="sm:mb-1"
             >
               <circle cx="12" cy="12" r="10" />
               <circle cx="12" cy="12" r="1" />
             </svg>
-            Single Select
+            <span className="text-xs">Single Select</span>
           </TabsTrigger>
-          <TabsTrigger value="multi_select" className="gap-1">
+          <TabsTrigger
+            value="multi_select"
+            className="flex flex-col items-center justify-center gap-1"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -71,16 +83,16 @@ export default function Home() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-copy-check"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="sm:mb-1"
             >
               <path d="m12 15 2 2 4-4" />
               <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
               <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
             </svg>
-            Multi Select
+            <span className="text-xs">Multi Select</span>
           </TabsTrigger>
         </TabsList>
         <TabsContent value="yes_no">
