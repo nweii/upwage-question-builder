e5d285c6eb45e09c91e6d338b24628c5b3942403
Nathan Cheng <nthn.wei@gmail.com>
2024-08-13 11:58:35 -0400

    feat: prevent indexing

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="robots" content="noindex, nofollow" />
         <link
           href="/upwage-favicon.png"
           rel="shortcut icon"
